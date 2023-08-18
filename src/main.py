5472f5f1e64bbd0bb9e6aec8d51569e0f6d112cd
AllanCapistrano <allan.capistrano3@gmail.com>
2023-08-17 21:39:51 -0300

    style: formatting code

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ from LexicalAnalyzer import LexicalAnalyzer
 def main():
     current_directory: Path = Path(__file__).parent.resolve()
     path: str = f"{current_directory}/files/"
+
     file_handler: FileHandler = FileHandler(path)
     lexical_analyzer: LexicalAnalyzer = LexicalAnalyzer()
-    file_names: List[str] = file_handler.get_file_names()
 
-    for file_name in file_names:
+    for file_name in file_handler.get_file_names():
         tokens = lexical_analyzer.generate_tokens(path, file_name)
         file_handler.write_file(file_name, tokens)
 
